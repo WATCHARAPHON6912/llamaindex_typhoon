aa29465aea11e1891e03ae59e9e1635d04f9ec1e
Watcharaphon pamayayang <110533343+WATCHARAPHON6912@users.noreply.github.com>
2024-07-09 21:18:47 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="llamaindex_typhoon",
-    version="0.0.1",
+    version="0.0.2",
     # description="A Modbus communication module for Film69 devices",
     py_modules=["typhoon"],  # ต้องตรงกับชื่อไฟล์ Modbus_Film69.py
     install_requires=[
